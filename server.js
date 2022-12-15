aff03cf12a21295b9b83f0210990d2fa3131a7a3
Brendon Silva <brendon.dasilva03@gmail.com>
2022-12-15 17:58:14 -0300

    Project details

--- server.js
+++ server.js
@@ -4,8 +4,29 @@ import Application from './src/Application'
 
 const context = Nullstack.start(Application)
 
-context.start = async function start() {
-  // https://nullstack.app/application-startup
+context.start = function () {
+  const { project } = context;
+  project.name = 'General Game';
+  project.shortName = 'General';
+  project.domain = 'nullstack.app';
+  project.color = '#d22365';
+  project.backgroundColor = '#d22365';
+  project.type = 'website';
+  project.display = 'standalone';
+  project.orientation = 'portrait';
+  project.scope = '/';
+  project.root = '/';
+  project.icons = {
+    '72': '/icon-72x72.png',
+    '128': '/icon-128x128.png',
+    '512': '/icon-512x512.png'
+  };
+  project.favicon = '/favicon.png';
+  project.disallow = ['/admin'];
+  project.sitemap = true;
+  project.cdn = 'cdn.nullstack.app';
+  project.protocol = 'https';
 }
+
 
 export default context
